155941ace9d80aef11d501cb466629da798b9985
Andrade1221 <111130558+Andrade1221@users.noreply.github.com>
2022-11-03 12:23:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+
+<head>
+
+<meta charset="UTF-8">
+<meta name="description" content="do your calculations">
+<meta name="keywords" content="Classroom, Teacher, Calculator, Activity">
+<meta name="author" content="Gabriel Andrade">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Calculator</title>
+<link rel="icon" type="image/x-icon" href="Icon.png">
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+<?php echo "Ola mundo"; ?>
+
+</body>
+
+</html>
